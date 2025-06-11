f7acf00cd206977ee0be80449078d70d7625032e
MBANS8A1 <seanmbng@aol.com>
2025-06-11 18:59:24 +0100

    Added a Handler and Looper class to the MainActivity.kt; this allows an Intent runnable with the help of the postDelayed() method to be run after 3 seconds when added to the messaging queue. The runnable will be run on the thread the Handler class is attached to.

--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
